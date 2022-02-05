1ca9863f33e3be073b0c1c2c676cac23070baa59
olaleye <olaoduola01@gmail.com>
2022-02-05 11:21:03 +0000

    change route to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/art", to: "art#index"
+  root "home#index"
+  get "/home", to: "home#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
