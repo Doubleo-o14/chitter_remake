5a7090e963eadc63e34a5258fb38db3732627fea
olaleye <olaoduola01@gmail.com>
2022-02-08 16:44:34 +0000

    added login

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,3 +5,11 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 end
+RSpec.describe HomeController, type: :controller do
+  describe "GET /new " do
+    it "responds with 200" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+end
